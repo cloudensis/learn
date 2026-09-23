9cf7b261eced893f90b8d67f74c1b4cedd9f1c25
kanaru-ssk <kanaru.ssk@gmail.com>
2026-09-23 11:23:58 +0900

    文言変更
    ソフトウェア開発を学ぶ人 -> ソフトウェア開発を学ぶ人

--- src/domains/company/constants.ts
+++ src/domains/company/constants.ts
@@ -1,5 +1,5 @@
 export const site = {
 	name: "cloudensis learn",
 	url: "https://learn.cloudensis.com",
-	description: "ソフトウェア開発を学ぶ人のための、無料の学習教材です。",
+	description: "ソフトウェア開発者のための、無料の学習教材です。",
 } as const;
