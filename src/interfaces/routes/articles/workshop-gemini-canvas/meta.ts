83928b891596f379a9a97d44626643b768a85c1a
kanaru-ssk <kanaru.ssk@gmail.com>
2026-09-23 13:07:29 +0900

    Update meta.ts

--- src/interfaces/routes/articles/workshop-gemini-canvas/meta.ts
+++ src/interfaces/routes/articles/workshop-gemini-canvas/meta.ts
@@ -2,9 +2,9 @@ import type { Article } from "#/src/domains/article/article";
 
 const meta: Article = {
 	slug: "workshop-gemini-canvas",
-	title: "ワークショップ：AIでゲームを作って、作りたいものへの道のりを描く",
+	title: "【EPIワークショップ】Geminiでゲームを作ってみよう",
 	description:
-		"Gemini の Canvas 機能でゲームを作ってみんなで遊び、自分が作りたいもののお試し版を作って、それを本物にするための学習ロードマップをAIと一緒に作るワークショップです。",
+		"GeminiのCanvas機能でゲームを作ってみる体験型ワークショップです。後半では自分で作りたいものを構想し、そのために必要な今後の学習ロードマップを作成します。",
 	publishedAt: "2026-09-23",
 	noindex: true,
 };
